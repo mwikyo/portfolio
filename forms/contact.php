6a6920036b41d55298c515682b4aed95963adb40
Derrick Daniel Mwikyo <50372816+mwikyo@users.noreply.github.com>
2023-04-25 02:48:55 +0300

    another php edit

--- forms/contact.php
+++ forms/contact.php
@@ -1,30 +1,15 @@
 <?php
+  $to_email = "mwikyoderrick@gmail.com"; // replace with your email address
+  $subject = "New message from website contact form";
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $headers = "From: $name <$email>";
 
-$receiving_email_address = 'mwikyoderrick@gmail.com';
-
-if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  include( $php_email_form );
-} else {
-  die( 'We are unable to send this email right now!');
-}
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-
-$contact->add_message( $_POST['name'], 'From');
-$contact->add_message( $_POST['email'], 'Email');
-$contact->add_message( $_POST['message'], 'Message', 10);
-
-$headers = "From: " . $contact->from_name . " <" . $contact->from_email . "> \r\n";
-$headers .= "Reply-To: " . $contact->from_name . " <" . $contact->from_email . "> \r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-mail($contact->to, 'New Contact Form Submission', $contact->message_html, $headers);
-
-echo 'Message sent!';
+  // send email
+  if (mail($to_email, $subject, $message, $headers)) {
+    echo "success";
+  } else {
+    echo "error";
+  }
 ?>
